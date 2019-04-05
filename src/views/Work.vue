64c057feeac2a923901aba72e6d2ada147b753df
Tenyson Partridge <tenysonpartridge@gmail.com>
2019-04-05 11:08:42 -0600

    added new carousel that performs better

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,29 +1,63 @@
 <template>
   <div class="work">
-
+    <slick class="slick" ref="slick" :options="slickOptions">
+      <div class="img-wrapper-one"></div>
+    </slick>
   </div>
 </template>
 
 <script>
+import Slick from 'vue-slick';
 
 export default {
+  components: Slick,
   data () {
     return {
+      slickOptions: {
+        slidesToShow: 1,
+        infinite: true,
+        accessibility: true,
+        adaptiveHeight: false,
+        arrows: true,
+        dots: true,
+        draggable: true,
+        edgeFriction: 0.30,
+        swipe: true
+      },
     }
   },
   methods: {
+
   },
+  mounted() {
+
+  }
 }
 </script>
 
 <style lang="scss">
 .work {
-  display: flex;
-  position: relative;
-  justify-content: center;
-  align-items: center;
+  // display: flex;
+  // position: relative;
+  // justify-content: center;
+  // align-items: center;
   width: 100%;
   height: 100%;
   max-width: 100%;
+  .slick {
+    width: 100%;
+    height: 100%;
+    max-width: 100%;
+    overflow: hidden;
+    .img-wrapper-one {
+      background: url('../assets/syphix-logo-bg.jpg');
+      background-size: cover;
+      background-position: center center;
+      background-repeat: no-repeat;
+      width: 100%;
+      max-width: 100%;
+      height: 100%;
+    }
+  }
 }
 </style>
